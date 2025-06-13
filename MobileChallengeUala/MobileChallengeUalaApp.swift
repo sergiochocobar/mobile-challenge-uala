ab5ac598ff2d6ec137a2a352b04b496853c24db6
Sergio Chocobar <sergiochocobar2@gmail.com>
2025-06-12 23:37:23 -0300

    Initial Commit

---
+++ MobileChallengeUala/MobileChallengeUalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileChallengeUalaApp.swift
+//  MobileChallengeUala
+//
+//  Created by NaranjaX on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MobileChallengeUalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
